b23513e2b25db53e2c03ed6cf38d7de707010613
itcuna0617 <itcuna0617@gmail.com>
2024-10-11 16:40:04 +0900

    commit

---
+++ Ex12_Food/src/main/java/com/sample/spring/repository/MenuRepository.java
@@ -0,0 +1,13 @@
+package com.sample.spring.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sample.spring.model.MenuEntity;
+
+public interface MenuRepository extends JpaRepository<MenuEntity, Long> {
+
+	List<MenuEntity> findAllByFoodId(Long foodId);
+
+}
